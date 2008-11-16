7334177190654e70806e0712fbf59312e63ebc55
unknown <mail@marc-seeger.de>
2008-11-16 11:06:12 +0100

    added a bit of comments and cleaned up 1 or 2 things

--- main.rb
+++ main.rb
@@ -9,23 +9,21 @@ get '/' do
 end
 
 get '/list' do
-	
-	@gpx_files = Dir['public/gpx/*.gpx'].sort.reverse
-	
-	@gpx_files.each do |file|
-	end
-	
+	#First we get all the gpx files in the gpx directory
+    #As they are named e.g. "2008-09-30_14-36-52.gpx", we only have to sort and reverse them to get the newer ones on top
+    @gpx_files = Dir['public/gpx/*.gpx'].sort.reverse
+    #Now let's enter the list.haml view
 	haml :list
 end
 
 get '/view/*' do
-	@filename = params["splat"]
-    @gpx = GpxStats.new("public/gpx/" + @filename.to_s)
+    #As the cached stuff doesn't seem to be recognized otherwise
 	mime :gpx, 'text/html'
+    #We get the filename that is attached to the view
+    @filename = params["splat"]
+    #And parse the file in question, this will take the longest time of them all
+    #Will only be called if the file isn't in cache yet!
+    @gpx = GpxStats.new("public/gpx/" + @filename.to_s)
+    #Now we go into the details.haml view, probably cached thanks to lib/caching.rb (look at the bottom of the resulting pages sourcecode for caching comment)
     cache(haml :details)
 end
-
-get '/clearcache' do
-#File.delete("public/view/")
-end
-
